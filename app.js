992b9699038787640118f282456af6990e6f51bb
Jonathan Seow <jseow5177@gmail.com>
2020-01-27 15:30:26 +0800

    Make changes for redeployment

--- app.js
+++ app.js
@@ -148,6 +148,6 @@ app.post("/delete-list", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server is listening at port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is listening at port");
 });
